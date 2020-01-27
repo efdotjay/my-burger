1e8955785d1378933e9708ba5f7fed1bf1a2adf8
Farhan <efdotjay@gmail.com>
2020-01-28 02:44:48 +0500

    Removes unnecessary render on modal.

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -9,19 +9,27 @@ type ModalProps = {
   onClose: () => void;
 };
 
-const Modal: React.FC<ModalProps> = props => (
-  <Fragment>
-    <Backdrop show={props.show} onClick={props.onClose}/>
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? 1 : 0
-      }}
-    >
-      {props.children}
-    </div>
-  </Fragment>
-);
+class Modal extends React.Component<ModalProps> {
+  shouldComponentUpdate(nextProps: ModalProps) {
+    return nextProps.show != this.props.show;
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Backdrop show={this.props.show} onClick={this.props.onClose}/>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? 1 : 0
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Fragment>
+    );
+  }
+}
 
 export default Modal;
